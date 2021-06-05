0f03cd110150aa80394e785e39c5b5db39236682
annaisha-sys <83236719+annaisha-sys@users.noreply.github.com>
2021-06-05 02:49:47 -0400

    setting up springboot template.

--- src/main/java/com/revature/repos/UserRepository.java
+++ src/main/java/com/revature/repos/UserRepository.java
@@ -7,9 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 
-    //@Query("select u from user where u.username = ?1")
-    User findByUsername(String username);
 
 }
-
-//provides a lot of CRUD operations by default, and one can use it to create their own query methods as well.
