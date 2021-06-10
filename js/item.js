a3be63e434a8e2c6085c7e492094973fe08b6b61
annaisha-sys <83236719+annaisha-sys@users.noreply.github.com>
2021-06-10 18:24:11 -0400

    updates

--- js/item.js
+++ js/item.js
@@ -16,3 +16,66 @@ GET from API
     - userComment - textArea
     - tradeable - checkbox
 */
+
+
+//CollectionItem FORM INPUTS
+const collectionItem = document.getElementById('collectionItem')
+const collectionItemOwned = document.getElementById('collectionItemOwned')
+const collectionItemWatched = document.getElementById('collectionItemWatched')
+const collectionItemUserRating = document.getElementById('collectionItemUserRating')
+const collectionItemUserComment = document.getElementById('collectionItemUserComment')
+const collectionItemTradeable = document.getElementById('collectionItemTradeable')
+
+const shouldNavigateAway = false;
+
+const handleCollectionItemSubmit = async (event) => {
+    event.preventDefault();
+
+    let collectionItemData = {};
+
+    collectionItemData.owned = collectionItem.value;
+    collectionItemData.watched = collectionItemWatched.value;
+    collectionItemData.userrating = collectionItemUserRating.value;
+    collectionItemData.usercomment = collectionItemUserComment.value; //must be trim if optional input?
+    collectionItemData.usertradeable = collectionItemTradeable.value;
+
+    console.log(collectionItemData);
+}
+
+
+const validateInputs = () => {
+    let isValid = true;
+    //add form after document
+    //may be done by checkboxes, if not will continue with rest.
+    var itemOwned = document.itemOwned.value;   
+    var userRating = document.userRating.value;
+
+
+    if(itemOwned === isValid){
+         return 1;
+    } else if((!collectionItemOwned.value) = isValid){
+        return 0;
+    }
+
+    isValid ? collectionTypeSubmit.removeAttribute('disabled') : collectionTypeSubmit.setAttribute('disabled', true);
+
+    if(userRating < 1 || userRating > 10 ) {
+        alert("That is not a valid entry.")
+        return false;
+    } return (true);
+
+
+}
+
+
+
+//EVENT LISTENERS
+
+document.querySelectorAll('input').forEach(element => element.addEventListener("input", validateInputs));
+collectionItemSubmit.addEventListener('click', function (event) {
+    // shouldNavigateAway;
+    handleCollectionItemSubmit(event);
+
+
+})
+
